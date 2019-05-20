b975bc4b73b7e15fd63a46372ec90713462aa6cf
jesperlofgren999 <jesperlofgren999@users.noreply.github.com>
2019-05-20 14:20:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>FilmFörsäkring</title>
+    <title>FilmFörsäkring TEST</title>
     <script src="assets/js/app.js"></script>
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
