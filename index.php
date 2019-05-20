9255bdd3cfe65ba1432b83545ffa7eb94bd907ac
Hannes Samskog <hannessamskog@gmail.com>
2019-05-20 13:54:56 +0200

    Html Boilerplate

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>FilmFörsäkring</title>
+    <script src="assets/js/app.js"></script>
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+<body>
+
+</body>
 
 </html>
