1934df3668c3237276f7364ced9e0206e5dc1bbd
JuliaMalmliden <50831294+JuliaMalmliden@users.noreply.github.com>
2019-05-27 13:28:51 +0200

    index
    fixat koppling till cssen

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
 
+
 <body>
+   <div class="container" class="pagehead">
+    <h1 Id="pagetitle"> FilmFörsäkring.se</h1>
+</div>
+
+<div class="topnav">
+  <a class="active" href="home.php">Home</a>
+  <a href="login.php">Log in</a>
+  <a href="register.php">Register</a>
+  <a href="question.php">Send in Questions</a>
+  <a href="about.php">About</a>
+  <a href="log_out.php">Log out</a>  
+</div>
 
 </body>
 
