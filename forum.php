081e3f1d9e685f55d552de96d2735089c1bace18
jesperlofgren999 <jesperlofgren999@users.noreply.github.com>
2019-05-28 13:57:26 +0200

    jl

---
+++ forum.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>FilmFörsäkring</title>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/app.js"></script>
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+
+<body>
+<div class="topnav">
+        <a href="index.php">Hem</a>
+        <a href="login.php">Logga in</a>
+        <a class="active" href="register.php">Registrera dig</a>
+        <a href="forum.php">Forum</a>
+        <a href="loggout.php">Logga ut</a>
+</div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="title">
+        <h1> Forum </h1>
+
+    </div>
+    
+
+    <footer>
+        <div class="footer">
+            <h2> FilmFörsäkring AB </h2>
+            <h3> since 2019 </h3>
+        </div>
+    </footer>
+</body>
+
+</html>
