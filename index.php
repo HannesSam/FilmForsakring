64e2aa76e5265fa768fd88dc1bb6059739a34c0d
Hannes Samskog <hannessamskog@gmail.com>
2019-05-28 13:17:09 +0200

    jag förstörde din kod julia

--- index.php
+++ index.php
@@ -38,12 +38,12 @@
         <p id="summary"> </p>
     </div>
 
+    <footer>
+        <div class="footer">
+            <h2> FilmFörsäkring AB </h2>
+            <h3> since 2019 </h3>
+        </div>
+    </footer>
 </body>
-<footer>
-    <div class="footer">
-        <h2> FilmFörsäkring AB </h2>
-        <h3> since 2019 </h3>
-    </div>
-</footer>
 
 </html>
