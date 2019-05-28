51272934239801eb7678bf1308f9affe720e478f
JuliaMalmliden <50831294+JuliaMalmliden@users.noreply.github.com>
2019-05-28 13:48:23 +0200

    topnav

--- login.php
+++ login.php
@@ -13,8 +13,8 @@
 <body>
 <div class="topnav">
         <a href="index.php">Hem</a>
-        <a href="login.php">Logga in</a>
-        <a class="active" href="register.php">Registrera dig</a>
+        <a class="active" href="login.php">Logga in</a>
+        <a href="register.php">Registrera dig</a>
         <a href="forum.php">Forum</a>
         <a href="loggout.php">Logga ut</a>
 </div>
