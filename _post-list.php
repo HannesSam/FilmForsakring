e51131ab736eced9bb5738076bc04f5dad68e038
JuliaMalmliden <50831294+JuliaMalmliden@users.noreply.github.com>
2019-05-29 14:33:41 +0200

    jajaa

--- _post-list.php
+++ _post-list.php
@@ -6,11 +6,18 @@ $result = Post::getPosts($title);
 while ($row = $result->fetch_assoc()) {
     $userID = $row["userID"];
     $email =  User::getUserName($userID);
-    echo '<div class="popUp"">
+    echo '<div class="popUp2"">
         <div class="reviewContainer">
-        <h2 class="title">' . $row["title"] . '</h2> <br> <br>' .
-        '<p>' . $row["stars"] . '</p>' .
-        '<p class="email">  Author:' . $email['email'] . '</p> <br>
-        <p class="review">'
-        . $row["review"] . '</p> </div> </div>';
+
+        '<p>' . $row["stars"] . '</p>' . 
+
+        '<p class="review">'
+        . $row["review"] . 
+        '</p> <br>
+
+        <p class="email">  
+        Skrivet av: ' . $email['email'] . 
+        '</p> <br>
+
+        </div> </div>';
 }
